485458115a9402873942cc040d30ae58b03165b3
Voopsy <tannenberg2003@gmail.com>
2022-06-26 17:49:28 +0300

    logo

--- MyProject/resources/views/layouts/app.blade.php
+++ MyProject/resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
 <div class="container">
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
         <a href="{{ route('home') }}" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-            <div class="hexagon"></div>
+            <div class="circle"></div>
             <span>Новости</span>
         </a>
 
